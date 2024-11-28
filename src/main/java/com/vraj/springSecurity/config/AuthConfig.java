785c462be82ab3f168b8e6d4a93674c83d387c38
RAY-29 <vibhuraj23@gmail.com>
2024-11-28 19:54:51 +0530

    Changes for Transactional and Lombok

---
+++ src/main/java/com/vraj/springSecurity/config/AuthConfig.java
@@ -0,0 +1,9 @@
+//package com.vraj.springSecurity.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class AuthConfig extends WebSecurityConfigurerAdapter{
+//}
